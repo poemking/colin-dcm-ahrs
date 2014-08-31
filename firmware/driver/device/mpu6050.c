b1c8609e32940693ace7667e575dc39781a43f35
Shengwen Cheng <shengwen1997.tw@gmail.com>
2014-09-01 00:26:31 +0800

    Fix the mpu6050 read who am i is not working issue

--- firmware/driver/device/mpu6050.c
+++ firmware/driver/device/mpu6050.c
@@ -1,52 +1,19 @@
 #include "stm32f4xx_conf.h"
 #include "i2c.h"
 
-#define MPU6050_DEVICE_ADDRESS 0x68
-
-/* I2C Timeout exception */
-typedef enum {I2C_SUCCESS, I2C_TIMEOUT} I2C_Status;
-int i2c_timeout;
-I2C_Status eeprom_i2c_status;
-#define I2C_TIMED(x) i2c_timeout = 0xFFFF; eeprom_i2c_status = I2C_SUCCESS; \
-while(x) { if(i2c_timeout-- == 0) { eeprom_i2c_status = I2C_TIMEOUT; goto i2c_restart;} }
+#define MPU6050_DEVICE_ADDRESS 0x68 << 1
 
 uint8_t mpu6050_read_who_am_i()
 {
-
-	if(I2C_GetFlagStatus(I2C1, I2C_FLAG_BUSY))
-		return 0;
-
 	uint8_t who_am_i_data = 0;
 
-	/* I2C start signal */
-	I2C_GenerateSTART(I2C1, ENABLE);
+	i2c_write_start(I2C1, MPU6050_DEVICE_ADDRESS);
+	i2c_write(I2C1, 0x75);
+	i2c_stop(I2C1);
 
-	I2C_TIMED(!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_MODE_SELECT));
-
-	/* Send the device address as slave address */
-	I2C_Send7bitAddress(I2C1, MPU6050_DEVICE_ADDRESS, I2C_Direction_Transmitter);
-
-	/* Transmit the WHO_AM_I address */
-	I2C_TIMED(!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED));
-
-	I2C_SendData(I2C1, 0x75);
-
-	I2C_TIMED(! I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_BYTE_TRANSMITTED));
-
-	/* Receive the WHO_AM_I data with NO ACK Message */
-	I2C_AcknowledgeConfig(I2C1, DISABLE);
-
-	I2C_TIMED(!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_BYTE_RECEIVED));
-
-	who_am_i_data = I2C_ReceiveData(I2C1);
-
-	/* I2C stop signal */
-	I2C_GenerateSTOP(I2C1, ENABLE);
-	I2C_TIMED(I2C1->CR1 & I2C_CR1_STOP);	
-
-	i2c_restart:
-	I2C_AcknowledgeConfig(I2C1, DISABLE);
-	I2C_AcknowledgeConfig(I2C1, ENABLE);
+	i2c_read_start(I2C1, MPU6050_DEVICE_ADDRESS);
+	who_am_i_data = i2c_read_nack(I2C1);
+	i2c_stop(I2C1);
 
 	return who_am_i_data;
 }
