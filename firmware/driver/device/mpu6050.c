228c6d249eb32076543e86979131e7a5c2cf660a
Shengwen Cheng <shengwen1997.tw@gmail.com>
2014-09-01 21:01:31 +0800

    Add the comment for MPU6050 configuration

--- firmware/driver/device/mpu6050.c
+++ firmware/driver/device/mpu6050.c
@@ -17,7 +17,9 @@ int mpu6050_init()
 	/* Check MPU6050 device is alive or not */
 	if(mpu6050_read_who_am_i() != 0x68) return 1;
 
+	//MPU6050 gyroscope : +-2000dps mode
 	i2c_single_write(I2C1, MPU6050_DEVICE_ADDRESS, MPU6050_GYRO_CONFIG, 0x18);
+	//MPU6050 accelerator : +-4g mode
 	i2c_single_write(I2C1, MPU6050_DEVICE_ADDRESS, MPU6050_ACCEL_CONFIG, 0x08);
 
 	return 0;
