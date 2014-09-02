2b7830f86312d00f7ceb993255cb9ceb8ad6984a
Shengwen Cheng <shengwen1997.tw@gmail.com>
2014-09-02 20:15:38 +0800

    Reset the power of MPU6050 while booting up

--- firmware/driver/device/mpu6050.c
+++ firmware/driver/device/mpu6050.c
@@ -36,6 +36,8 @@ int mpu6050_init()
 	/* Check MPU6050 device is alive or not */
 	if(mpu6050_read_who_am_i() != 0x68) return 1;
 
+	//Reset the device
+	i2c_single_write(I2C1, MPU6050_DEVICE_ADDRESS,	MPU6050_PWR_MGMT_1, 0x80);
 	//MPU6050 gyroscope : +-2000dps mode
 	i2c_single_write(I2C1, MPU6050_DEVICE_ADDRESS, MPU6050_GYRO_CONFIG, 0x18);
 	//MPU6050 accelerator : +-4g mode
