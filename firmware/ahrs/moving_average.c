f445497317ece094aabbf54025be4ccde93de576
Shengwen Cheng <shengwen1997.tw@gmail.com>
2014-09-07 12:43:56 +0800

    Implement the Exponential Moving Average filter

--- firmware/ahrs/moving_average.c
+++ firmware/ahrs/moving_average.c
@@ -1,4 +1,5 @@
 #include <stdint.h>
+#include "arm_math.h"
 #include <string.h>
 
 #include "vector_space.h"
@@ -58,4 +59,46 @@ void vector3d_weight_moving_average(vector3d_f_t new_sampling_data, vector3d_f_t
 	data_result->x /= (sampling_count * (sampling_count + 1) / 2);
 	data_result->y /= (sampling_count * (sampling_count + 1) / 2);
 	data_result->z /= (sampling_count * (sampling_count + 1) / 2);
+}
+
+void vector3d_exponential_moving_average(vector3d_f_t new_sampling_data, vector3d_f_t *data_fifo,
+	vector3d_f_t *data_result, int sampling_count)
+{
+	float alpha, alpha_sum = 0;
+
+	int i;
+	for(i = 0; i < (sampling_count - 1); i++) {
+		//Drop the old data
+		memcpy((uint8_t *)&data_fifo[i], (uint8_t *)&data_fifo[i + 1], sizeof(vector3d_f_t));
+
+		//Calculate the alpha value
+		alpha = 2 / (i + 2);
+
+		//Calculate the alpha sum
+		alpha_sum += pow(alpha, i + 1);
+
+		//weight the old data without last time dropped then calculate the sum
+		data_result->x += data_fifo[i].x * alpha;
+		data_result->y += data_fifo[i].y * alpha;
+		data_result->z += data_fifo[i].z * alpha;
+	}
+
+	/* Put new data at the last of the FIFO */
+	memcpy((uint8_t *)&data_fifo[sampling_count -1], (uint8_t *)&new_sampling_data, sizeof(vector3d_f_t));
+
+	//Calculate the alpha value
+	alpha = 2 / (sampling_count + 1);
+
+	//Calculate the alpha sum
+	alpha_sum += pow(alpha, sampling_count);
+
+	//Add the new data into the FIFO sum
+	data_result->x += new_sampling_data.x * alpha;
+	data_result->y += new_sampling_data.y * alpha;
+	data_result->z += new_sampling_data.z * alpha;
+
+	/* Caluculate the average of the new FIFO */
+	data_result->x /= alpha_sum;
+	data_result->y /= alpha_sum;
+	data_result->z /= alpha_sum;
 }
