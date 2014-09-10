1bef2b61ec9579d9b8dfe86fca22ab8b5203cc15
Shengwen Cheng <shengwen1997.tw@gmail.com>
2014-09-10 22:19:23 +0800

    Fix the LED operating issue for QCopter Flight Control Board

--- firmware/driver/peripheral/led.c
+++ firmware/driver/peripheral/led.c
@@ -1,16 +1,22 @@
 #include "stm32f4xx.h"
+#include "led.h"
 
 void led_init()
 {
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOC, ENABLE);
+
 	GPIO_InitTypeDef GPIO_InitStruct = {
-		.GPIO_Pin = GPIO_Pin_13 | GPIO_Pin_14,
+		.GPIO_Pin = GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15,
 		.GPIO_Mode = GPIO_Mode_OUT,
 		.GPIO_Speed = GPIO_Speed_50MHz,
 		.GPIO_OType = GPIO_OType_PP,
-		.GPIO_PuPd = GPIO_PuPd_NOPULL
+		.GPIO_PuPd = GPIO_PuPd_DOWN
 	};
 
-	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOG, ENABLE);
+	GPIO_Init(GPIOC, &GPIO_InitStruct);
 
-	GPIO_Init(GPIOG, &GPIO_InitStruct);
+	/* Turn off the led */
+	led_off(LED1);
+	led_off(LED2);
+	led_off(LED3);
 }
--- firmware/driver/peripheral/led.h
+++ firmware/driver/peripheral/led.h
@@ -2,11 +2,17 @@
 #define __LED_H
 
 #define led_toggle(led)  GPIO_ToggleBits(led)
-#define led_off(led)  GPIO_ResetBits(led)
-#define led_on(led)  GPIO_SetBits(led)
+/* 
+ * <For QCopter Flight Control Board>
+ * Because there are three push-up register in the circuit for the LED
+ * The GPIO high/low operation to the LED will be opposite
+ */
+#define led_off(led)  GPIO_SetBits(led)
+#define led_on(led)  GPIO_ResetBits(led)
 
-#define LED1 GPIOG, GPIO_Pin_13
-#define LED2 GPIOG, GPIO_Pin_14
+#define LED1 GPIOC, GPIO_Pin_15
+#define LED2 GPIOC, GPIO_Pin_14
+#define LED3 GPIOC, GPIO_Pin_13
 
 void led_init();
 
