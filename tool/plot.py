4853585f5cb115bbe7728020c10fe7bfbca93997
Shengwen Cheng <shengwen1997.tw@gmail.com>
2014-09-12 21:40:07 +0800

    Support plotting the data in realtime

--- tool/plot.py
+++ tool/plot.py
@@ -1,10 +1,12 @@
 from sys import argv
 from matplotlib import pyplot as plt
+from collections import deque
 import serial
 import struct
+import numpy as np
 
 #Serial port defalut settings
-port = '/dev/ttyUSB0'
+port = '/dev/ttyUSB1'
 baudrate = 57600
 _serial = serial.Serial(port, baudrate, timeout = 1024)
 
@@ -51,41 +53,83 @@ def read_new_data():
 	
 	return 'success'
 
-def matlab_plot_init():
-	plt.ion()
-	plt.figure(figsize=(10,6))
+class AnalogPlot:
+	def __init__(self, plot_data_max_count):
+		self.plot_data_max_count = plot_data_max_count
 
-	#Subplot - accel data
-	plt.subplot(211)
-	plt.xlabel('Time')
-	plt.ylabel('Acceleration')
-	plt.plot(0, label='x-axis', color='red')
-	plt.plot(0, label='y-axis', color='blue')
-	plt.plot(0, label='z-axis', color='green')
-	plt.ylim([-4, 4])
-	plt.legend()
-	plt.grid()
-	plt.draw()
+		#Plot data, use ring buffer data structure
+		self.accel_raw_x_data = deque([0.0] * plot_data_max_count)
+		self.accel_raw_y_data = deque([0.0] * plot_data_max_count)
+		self.accel_raw_z_data = deque([0.0] * plot_data_max_count)
+		self.gyro_raw_x_data = deque([0.0] * plot_data_max_count)
+		self.gyro_raw_y_data = deque([0.0] * plot_data_max_count)
+		self.gyro_raw_z_data = deque([0.0] * plot_data_max_count)
 
-	#Subplot - gyro data
-	plt.subplot(212)
-	plt.xlabel('Time')
-	plt.ylabel('Degree per second')
-	plt.ylim([-2000, 2000])
-	plt.plot(0, label='x-axis', color='red')
-	plt.plot(0, label='y-axis', color='blue')
-	plt.plot(0, label='z-axis', color='green')
-	plt.legend()
-	plt.grid()
-	plt.draw()
+		plt.ion()
+		plt.figure(figsize=(10,6))
 
+		#Subplot - accel data
+		plt.subplot(211)
+		plt.xlabel('Time')
+		plt.ylabel('Acceleration')
+		self.accel_raw_x, = plt.plot(self.accel_raw_x_data, label='x-axis', color='red')
+		self.accel_raw_y, = plt.plot(self.accel_raw_y_data, label='y-axis', color='blue')
+		self.accel_raw_z, = plt.plot(self.accel_raw_x_data, label='z-axis', color='green')
+		plt.ylim([-4, 4])
+		plt.legend()
+		plt.grid()
+		plt.draw()
+
+		#Subplot - gyro data
+		plt.subplot(212)
+		plt.xlabel('Time')
+		plt.ylabel('Degree per second')
+		plt.ylim([-2000, 2000])
+		self.gyro_raw_x, = plt.plot(self.gyro_raw_x_data, label='x-axis', color='red')
+		self.gyro_raw_y, = plt.plot(self.gyro_raw_y_data, label='y-axis', color='blue')
+		self.gyro_raw_z, = plt.plot(self.gyro_raw_z_data, label='z-axis', color='green')
+		plt.legend()
+		plt.grid()
+		plt.draw()
+
+	def add_new_data(self, data, value):
+		if len(data) < self.plot_data_max_count:
+			data.append(val)
+		else:
+			data.pop()
+		data.appendleft(value)
+
+	def update(self):
+		#Get the new data and update the ring buffer
+		self.add_new_data(self.accel_raw_x_data, np.array(onboard_data_value[0]))
+		self.add_new_data(self.accel_raw_y_data, np.array(onboard_data_value[1]))
+		self.add_new_data(self.accel_raw_z_data, np.array(onboard_data_value[2]))
+		self.add_new_data(self.gyro_raw_x_data, np.array(onboard_data_value[3]))
+		self.add_new_data(self.gyro_raw_y_data, np.array(onboard_data_value[4]))
+		self.add_new_data(self.gyro_raw_z_data, np.array(onboard_data_value[5]))
+
+		#Plot the new data
+		self.accel_raw_x.set_ydata(self.accel_raw_x_data)
+		self.accel_raw_y.set_ydata(self.accel_raw_y_data)
+		self.accel_raw_z.set_ydata(self.accel_raw_z_data)
+		self.gyro_raw_x.set_ydata(self.gyro_raw_x_data)
+		self.gyro_raw_y.set_ydata(self.gyro_raw_y_data)
+		self.gyro_raw_z.set_ydata(self.gyro_raw_z_data)
+
+		plt.draw()
 
 def run():
-	matlab_plot_init()
+	analog_plot = AnalogPlot(200)
+	plot_prescaler = 0
 
 	while True:
 		if(read_new_data() != 'success'):
 			continue
+		plot_prescaler += 1
+
+		if(plot_prescaler == 10):
+			analog_plot.update()
+			plot_prescaler = 0
 
 		print "\033c"
 
