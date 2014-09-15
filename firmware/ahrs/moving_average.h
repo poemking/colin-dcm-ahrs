b7d2c3ba502fdbafddcaa1fd7d123d4170f54ab8
Shengwen Cheng <shengwen1997.tw@gmail.com>
2014-09-15 22:32:49 +0800

    Fix the EMA filter last data not been updated issue

--- firmware/ahrs/moving_average.h
+++ firmware/ahrs/moving_average.h
@@ -5,7 +5,7 @@ void vector3d_simple_moving_average(vector3d_f_t new_sampling_data, vector3d_f_t
 	vector3d_f_t *data_result, int sampling_count);
 void vector3d_weight_moving_average(vector3d_f_t new_sampling_data, vector3d_f_t *data_fifo,
 	vector3d_f_t *data_result, int sampling_count);
-void vector3d_exponential_moving_average(vector3d_f_t new_sampling_data, vector3d_f_t *last_sampling_data,
+void vector3d_exponential_moving_average(vector3d_f_t new_sampling_data, vector3d_f_t *last_ema_data,
 	vector3d_f_t *data_result, float alpha);
 
 #endif
