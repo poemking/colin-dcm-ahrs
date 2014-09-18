9d10adeb0a516cdce9bfe23510c36d3752a3c368
Shengwen Cheng <shengwen1997.tw@gmail.com>
2014-09-18 23:15:35 +0800

    Fix the C trigonometric function should be radian issue

--- firmware/ahrs/ahrs.c
+++ firmware/ahrs/ahrs.c
@@ -3,6 +3,8 @@
 #include "vector_space.h"
 #include "ahrs.h"
 
+#define deg_to_rad(angle) angle * 0.0174532925f //Angle * (PI / 180)
+
 void gyro_integrate(attitude_t *attitude, vector3d_f_t gyro_scaled_data,
 	float period_time)
 {
@@ -11,16 +13,16 @@ void gyro_integrate(attitude_t *attitude, vector3d_f_t gyro_scaled_data,
 	//x' = x + cos(roll)tan(pitch)z + sin(roll)tan(pitch)y
 	iframe_gyro.x = 
 		gyro_scaled_data.x +
-		cos(attitude->roll_angle) * tan(attitude->pitch_angle) * gyro_scaled_data.z +
-		sin(attitude->roll_angle) * tan(attitude->pitch_angle) * gyro_scaled_data.y;
+		cos(deg_to_rad(attitude->roll_angle)) * tan(deg_to_rad(attitude->pitch_angle)) * gyro_scaled_data.z +
+		sin(deg_to_rad(attitude->roll_angle)) * tan(deg_to_rad(attitude->pitch_angle)) * gyro_scaled_data.y;
 	//y' = cos(pitch)y - sin(roll)z
 	iframe_gyro.y =
-		cos(attitude->pitch_angle) * gyro_scaled_data.y -
-		sin(attitude->roll_angle) * gyro_scaled_data.z;
+		cos(deg_to_rad(attitude->pitch_angle)) * gyro_scaled_data.y -
+		sin(deg_to_rad(attitude->roll_angle)) * gyro_scaled_data.z;
 	//z' = cos(roll)sec(pitch)z + sin(roll)sec(pitch)y
 	iframe_gyro.z =
-		cos(attitude->roll_angle) * (1 / cos(attitude->pitch_angle)) * gyro_scaled_data.z +
-		sin(attitude->roll_angle) * (1 / cos(attitude->pitch_angle)) * gyro_scaled_data.y;
+		cos(deg_to_rad(attitude->roll_angle)) * (1 / cos(deg_to_rad(attitude->pitch_angle))) * gyro_scaled_data.z +
+		sin(deg_to_rad(attitude->roll_angle)) * (1 / cos(deg_to_rad(attitude->pitch_angle))) * gyro_scaled_data.y;
 
 	/* Intergrate the angle velocity */
 	attitude->roll_angle += iframe_gyro.x * period_time;
