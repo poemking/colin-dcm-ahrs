bd385dd7645b8470bfaffdc94379d9b2d49a9c96
Shengwen Cheng <shengwen1997.tw@gmail.com>
2014-09-20 21:47:10 +0800

    Create attitude_t / byte convert function

--- firmware/telemetry/telemetry.c
+++ firmware/telemetry/telemetry.c
@@ -5,6 +5,8 @@
 
 #include "vector_space.h"
 
+#include "ahrs.h"
+
 int convert_float_to_byte(float *float_data, uint8_t *byte_data)
 {
 	memcpy(byte_data, (uint8_t *)float_data, sizeof(float));
@@ -17,6 +19,13 @@ int convert_vector3d_float_to_byte(vector3d_f_t *vector_data, uint8_t *byte_data
 	memcpy(byte_data, (uint8_t *)vector_data, sizeof(vector3d_f_t));
 
 	return sizeof(vector3d_f_t);
+}
+
+int convert_attitude_to_byte(attitude_t *attitude_data, uint8_t *byte_data)
+{
+	memcpy(byte_data, (uint8_t *)attitude_data, sizeof(attitude_t));
+
+	return sizeof(attitude_t);
 }
 
 static uint8_t generate_checksum_byte(uint8_t *payload, int payload_count)
--- firmware/telemetry/telemetry.h
+++ firmware/telemetry/telemetry.h
@@ -3,8 +3,11 @@
 
 #include "vector_space.h"
 
+#include "ahrs.h"
+
 int convert_float_to_byte(float *float_data, uint8_t *byte_data);
 int convert_vector3d_float_to_byte(vector3d_f_t *vector_data, uint8_t *byte_data);
+int convert_attitude_to_byte(attitude_t *attitude_data, uint8_t *byte_data);
 
 void send_onboard_parameter(uint8_t *payload, int payload_count);
 
