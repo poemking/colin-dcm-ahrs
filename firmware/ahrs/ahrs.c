5f5944a53d533657863663d5b653ab7b8b0e12e8
Shengwen Cheng <shengwen1997.tw@gmail.com>
2014-10-06 21:57:06 +0800

    Add acceleration boundary in the adaptive complementry filter

--- firmware/ahrs/ahrs.c
+++ firmware/ahrs/ahrs.c
@@ -67,6 +67,11 @@ void gyro_error_eliminate(attitude_t *gyro_attitude, attitude_t accel_attitude, 
 	//float alpha_roll, alpha_pitch;
 	float beta;
 
+        /* Use SVM(Signal Vector Magnitude) to determine the reliablilty of accelerometer */
+	float accel_sma_value = sqrtf(accel_scaled_data.x * accel_scaled_data.x +
+		accel_scaled_data.y * accel_scaled_data.y +
+		accel_scaled_data.z * accel_scaled_data.z);
+
 	/* Use SVM(Signal Vector Magnitude) to determine the reliablilty of accelerometer */
 	float accel_sma_value = sqrtf(accel_scaled_data.x * accel_scaled_data.x +
 		accel_scaled_data.y * accel_scaled_data.y +
